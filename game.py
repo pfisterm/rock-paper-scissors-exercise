94b6bb5fad88d9610180d53a3c32376596b0eba0
pfisterm <pfisterm@users.noreply.github.com>
2021-09-17 20:42:31 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import random
 
-
+#Welcome
 print("Rock, Paper, Scissors, Shoot!")
+print("Welcome 'Player One' to my Rock-Paper_Scissors game ...")
 
 #User Choice
 user_choice = input("Please choose either 'rock', paper' or scissors:'")
@@ -18,3 +19,24 @@ computer_choice = random.choice(options)
 print(computer_choice)
 
 #Determining the Winner
+if(computer_choice == 'rock' and user_choice == 'scissors'):
+    print("You won!")
+elif(computer_choice == 'rock' and user_choice == 'paper'):
+    print("Oh, the computer won. It's ok.")
+elif(computer_choice == 'rock' and user_choice == 'rock'):
+    print("It's a tie!")
+elif(computer_choice == 'paper' and user_choice == 'scissors'):
+    print("You won!")
+elif(computer_choice == 'paper' and user_choice == 'paper'):
+    print("It's a tie!")
+elif(computer_choice == 'paper' and user_choice == 'rock'):
+    print("Oh, the computer won. It's ok.")
+elif(computer_choice == 'scissors' and user_choice == 'scissors'):
+    print("It's a tie!")
+elif(computer_choice == 'scissors' and user_choice == 'paper'):
+    print("Oh, the computer won. It's ok.")                        
+else:
+    print("You won!")    
+
+#Goodbye
+print("Thanks for playing. Please play again!")
